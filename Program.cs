80eae8a7148221c2ef9bab1a6e175419ee70c661
Furkan Cengiz <76488131+furkancengiz6@users.noreply.github.com>
2025-01-07 11:52:46 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+using OOP;
+
+
+//new kullanarak bir nesneler olusturuldu
+Person teacher = new Person("John", "Doe", new DateTime(1980, 1, 1));
+Person teacher1 = new Person("Jon", "o", new DateTime(1982, 5,6));
+Person student = new Person("Jane", "Doe", new DateTime(2000, 1, 1));
+//Info methodu cagırıldı
+teacher.Info();
+student.Info();
+
+
+
+
+
+
